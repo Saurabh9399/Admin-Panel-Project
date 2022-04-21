88b4cef658a9311ffa83bc23b296dbf42052defc
saurabh <saurabhj8087445@gmail.com>
2022-04-21 16:54:04 +0530

    add Sidebar folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-        
+        <Sidebar />
       </div>
     </div>
   );
